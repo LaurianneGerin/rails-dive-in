3a97cfd44bd7b775821d2f417590532fece6b058
lilian garrido <lilian.garrido13@gmail.com>
2016-08-15 18:38:00 +0200

    Add validation to User model, missing email regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+
+  has_many :pools, dependent: :destroy
+  has_many :reservations
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: true #add a regex
+  validates :password, uniqueness: true, presence: true
+
 end
