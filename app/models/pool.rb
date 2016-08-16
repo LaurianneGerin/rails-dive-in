6e40ceb034e64fac8649ef15e109f2868a871ab9
lilian garrido <lilian.garrido13@gmail.com>
2016-08-16 12:47:14 +0200

    Modify validations in pool Model

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -5,16 +5,11 @@ class Pool < ApplicationRecord
   validates :height, presence: true
   validates :depth, presence: true
   validates :color, presence: true
-  validates :pool_house, presence: true
-  validates :pool_toys, presence: true
   validates :pool_type, presence: true
   validates :address, presence: true
   validates :water_type, presence: true
   validates :capacity, presence: true
   validates :description, presence: true
-  validates :alcohol, presence: true
-  validates :soft, presence: true
-  validates :bbq, presence: true
-  validates :pool_girl, presence: true
+  validates :pool_girl, presence: true, default: 0
   validates :pool_boy, presence: true
 end
