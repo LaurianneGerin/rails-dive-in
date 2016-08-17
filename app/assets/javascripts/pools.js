054d01d8b44caeec327051e9e24c9e3419d7b5af
lilian garrido <lilian.garrido13@gmail.com>
2016-08-17 14:18:44 +0200

    Add the date picker plus city and and category picker, plus list of the pool

--- app/assets/javascripts/pools.js
+++ app/assets/javascripts/pools.js
@@ -2,21 +2,5 @@
 // All this logic will automatically be available in application.js.
 
 $('.input-daterange').datepicker({
-  todayBtn: "linked",
-  beforeShowDay: function (date){
-    if (date.getMonth() == (new Date()).getMonth())
-      switch (date.getDate()){
-        case 4:
-          return {
-            tooltip: 'Example tooltip',
-            classes: 'active'
-          };
-        case 8:
-          return false;
-        case 12:
-          return "green";
-    }
-  },
   toggleActive: true,
-  defaultViewDate: { year: 1977, month: 04, day: 25 }
 });
