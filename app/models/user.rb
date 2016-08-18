e66ceb5d6ca1953e7dff5a581201a8d3e6af45e0
Laurianne Gerin <laurianne.gerin@gmail.com>
2016-08-18 11:57:14 +0200

    Index paufine et boutton Accept , Delete and Deny MARCHE

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,14 @@ class User < ApplicationRecord
 
   has_attachment  :photo, accept: [:jpg, :png]
 
-
   # TODO validates :first_name, presence: true
   # TODO validates :last_name, presence: true
 
+  def list_own_pool_reservations
+    pool_reservations = []
+    self.pools.each do |pool|
+       pool_reservations << pool.reservations
+    end
+    pool_reservations.flatten
+  end
 end
