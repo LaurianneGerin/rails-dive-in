e3d81e9e5e114bf7a81207cade9157393da10d21
Laurianne Gerin <laurianne.gerin@gmail.com>
2016-08-19 14:53:01 +0200

    suppression du each sur l'index des pools, ajout du path photos sur l'index

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -45,7 +45,6 @@ class PoolsController < ApplicationController
       flash[:notice] = "Your pool has been successfully added !"
       redirect_to root_path
     else
-      flash[:notice] = "Your pool has not been successfully added..."
       render :new
     end
   end
