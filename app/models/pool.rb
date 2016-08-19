8031ab33e839dda71a132224d2f1724ccad566f2
lilian garrido <lilian.garrido13@gmail.com>
2016-08-19 16:23:57 +0200

    add footer fixed, set the show and put english everywhere

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -6,16 +6,12 @@ class Pool < ApplicationRecord
   validates :width, presence: true
   validates :height, presence: true
   validates :depth, presence: true
-  validates :color, presence: true
 
   validates :pool_type, presence: true
   validates :address, presence: true
   validates :water_type, presence: true
   validates :capacity, presence: true
   validates :description, presence: true
-
-  validates :pool_girl, presence: true
-  validates :pool_boy, presence: true
 
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
